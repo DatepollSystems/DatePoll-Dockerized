fb8fbc165c924ae73453e1f70c6833b7d3f3207a
Dominik <git@dafnik.me>
2020-05-25 18:24:36 +0200

    [ref] Version change code

--- scripts/updateDatePollBackend.sh
+++ scripts/updateDatePollBackend.sh
@@ -1,35 +1,41 @@
+version=master
+
+while getopts ":v:" opt; do
+  case $opt in
+    v)
+      echo "-v was triggered, Parameter: $OPTARG" >&2
+      if [ "$OPTARG" == "dev" ]
+      then
+        echo "> Using dev version..."
+        version=dev
+      fi
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument. Example: dev - dev version" >&2
+      exit 1
+      ;;
+  esac
+done
+
 read -p "Are you sure you want to upgrade DatePoll-Backend? [y/N] " prompt
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 then
     cd ./code/backend/
 
-    echo "> Fetching new version"
-
-    while getopts ":v:" opt; do
-	  case $opt in
-	    v)
-	      echo "-v was triggered, Parameter: $OPTARG" >&2
-	      if [ "$OPTARG" == "rl" ]
-	      then
-	        echo "> Fetching rl version..."
-	        git fetch origin
-			git reset --hard origin/master
-	      else
-	        echo "> Fetching dev version..."
-	        git fetch origin
-			git reset --hard origin/development
-	      fi
-	      ;;
-	    \?)
-	      echo "Invalid option: -$OPTARG" >&2
-	      exit 1
-	      ;;
-	    :)
-	      echo "Option -$OPTARG requires an argument. Example: dev - dev, dev..." >&2
-	      exit 1
-	      ;;
-	  esac
-	done
+	if [ version == "master" ]
+	then
+		echo "> Fetching master branch..."
+	    git fetch origin
+		git reset --hard origin/master
+	else
+	    echo "> Fetching development version..."
+	    git fetch origin
+		git reset --hard origin/development
+	fi
 	
     echo "> Done"
 
